d3a8f2becb12a4560340f47f9bcd4f62a4f9ebc4
gcwill70 <gcwill70@gmail.com>
2023-06-26 14:53:24 -0400

    dont need to prepend assets/ for filepaths only?

--- lib/constants/files.dart
+++ lib/constants/files.dart
@@ -1,6 +1,6 @@
 class Files {
   Files._();
 
-  static const String content = 'assets/files/content.md';
-  static const String portfolio = 'assets/files/portfolio.md';
+  static const String content = 'files/content.md';
+  static const String portfolio = 'files/portfolio.md';
 }
