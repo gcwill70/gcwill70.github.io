982ba2b99d665d7616421f438848bb1d1b38cae4
gcwill70 <gcwill70@gmail.com>
2023-06-26 14:29:21 -0400

    flatten assets folder

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -2,12 +2,12 @@ class Images {
   Images._();
 
   // Picture
-  static const String photo = 'assets/images/IMG_3191.JPG';
+  static const String photo = 'assets/IMG_3191.JPG';
 
   // social icons
-  static const String linkedin = 'assets/icons/ic_linkedin.png';
-  static const String github = 'assets/icons/ic_github.png';
-  static const String resume = 'assets/icons/ic_resume.png';
-  static const String twitter = 'assets/icons/ic_twitter.png';
-  static const String overleaf = 'assets/icons/ic_overleaf.png';
+  static const String linkedin = 'assets/ic_linkedin.png';
+  static const String github = 'assets/ic_github.png';
+  static const String resume = 'assets/ic_resume.png';
+  static const String twitter = 'assets/ic_twitter.png';
+  static const String overleaf = 'assets/ic_overleaf.png';
 }
