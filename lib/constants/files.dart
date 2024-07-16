7e3128b37d9adfb71441f6fed13f607f07f41889
Geoff Cramer <gcwill70@gmail.com>
2024-07-16 18:55:12 -0400

    remove flutter files

--- lib/constants/files.dart
+++
@@ -1,7 +0,0 @@
-class Files {
-  Files._();
-
-  static const String content = 'files/content.md';
-  static const String portfolio = 'files/portfolio.md';
-  static const String resume = 'assets/files/Resume_GeoffCramer.pdf';
-}
