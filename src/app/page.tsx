fa2b29ba8895c3d35516fd336818fa63437ff934
Geoff Cramer <gcwill70@gmail.com>
2024-07-16 18:41:36 -0400

    fix layout again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { SummaryView } from "@/summary/SummaryView";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 bg-gray-100 text-gray-900">
-      <div className="w-full flex flex-col lg:flex-row gap-4">
-        <div className="max-w-fit">
+      <div className="w-full flex flex-col md:flex-row gap-4">
+        <div className="max-w-fit hidden md:block">
           <SummaryView />
         </div>
         <div className="min-w-72">
