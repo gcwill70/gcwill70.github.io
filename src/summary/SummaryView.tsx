a5bec7aa96f1ffe887b34a372e705afb55749f88
Geoff Cramer <gcwill70@gmail.com>
2024-07-16 18:47:03 -0400

    add links to summary

--- src/summary/SummaryView.tsx
+++ src/summary/SummaryView.tsx
@@ -1,14 +1,26 @@
 export const SummaryView = () => {
   return (
-    <div className="flex flex-row md:flex-col">
+    <div className="flex flex-col gap-4">
       <div>
-        <img src="/images/IMG_3191.JPG" className="max-w-52" alt="photo of me" />
+        <img
+          src="/images/IMG_3191.JPG"
+          className="max-w-52"
+          alt="photo of me"
+        />
       </div>
       <div>
         <h1>Geoff Cramer</h1>
         <h2>Fullstack Developer</h2>
-        <h3>M.S. in Electrical & Computer Engineering</h3>
+        <h3>M.S. in Computer Engineering</h3>
         <h4>gcwill70@gmail.com</h4>
+      </div>
+      <div className="flex flex-row gap-3 items-center">
+        <a href="https://www.linkedin.com/in/geoff-cramer/" target="_blank">
+          <img src="/images/ic_linkedin.png" alt="LinkedIn" className="w-10" />
+        </a>
+        <a href="https://github.com/gcwill70" target="_blank">
+          <img src="/images/ic_github.png" alt="GitHub" className="w-10" />
+        </a>
       </div>
     </div>
   );
