48763297fab75df99412e98a5c29c7ba11f444db
Geoff Cramer <gcwill70@gmail.com>
2025-03-05 10:06:55 -0500

    improve layout and update profile photo

--- src/experiences/ExperiencesView.tsx
+++ src/experiences/ExperiencesView.tsx
@@ -5,7 +5,7 @@ export const ExperiencesView = () => {
         <h1 className="text-3xl font-bold">About Me</h1>
         <p>
           Hi there! I&apos;m Geoff and I&apos;ve been software engineer since
-          2018. I&apos; spent recent years working in web development and Trust
+          2018. I&apos;ve spent recent years working in web development and Trust
           &amp; Safety.
         </p>
       </div>
--- src/summary/SummaryView.tsx
+++ src/summary/SummaryView.tsx
@@ -1,26 +1,35 @@
 export const SummaryView = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <div>
-        <img
-          src="/images/IMG_3191.JPG"
-          className="max-w-52"
-          alt="photo of me"
-        />
-      </div>
-      <div>
-        <h1>Geoff Cramer</h1>
-        <h2>Fullstack Developer</h2>
-        <h3>M.S. in Computer Engineering</h3>
-        <h4>gcwill70@gmail.com</h4>
-      </div>
-      <div className="flex flex-row gap-3 items-center">
-        <a href="https://www.linkedin.com/in/geoff-cramer/" target="_blank">
-          <img src="/images/ic_linked_in.png" alt="LinkedIn" className="w-10" />
-        </a>
-        <a href="https://github.com/gcwill70" target="_blank">
-          <img src="/images/ic_github.png" alt="GitHub" className="w-10" />
-        </a>
+    <div className="flex flex-row xl:flex-col gap-4">
+      <img
+        src="/images/profile_square.JPG"
+        className="max-h-52 xl:hidden"
+        alt="photo of me"
+      />
+      <img
+        src="/images/profile_tall.JPG"
+        className="max-w-52 hidden xl:block"
+        alt="photo of me"
+      />
+      <div className="flex flex-col gap-2">
+        <div>
+          <h1>Geoff Cramer</h1>
+          <h2>Fullstack Developer</h2>
+          <h3>M.S. in Computer Engineering</h3>
+          <h4>gcwill70@gmail.com</h4>
+        </div>
+        <div className="flex flex-row gap-3">
+          <a href="https://www.linkedin.com/in/geoff-cramer/" target="_blank">
+            <img
+              src="/images/ic_linked_in.png"
+              alt="LinkedIn"
+              className="w-10"
+            />
+          </a>
+          <a href="https://github.com/gcwill70" target="_blank">
+            <img src="/images/ic_github.png" alt="GitHub" className="w-10" />
+          </a>
+        </div>
       </div>
     </div>
   );
