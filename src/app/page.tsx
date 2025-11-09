851f01d2c8745db07559dc47678e8bf4eac5dd27
gcwill70 <gcwill70@gmail.com>
2025-11-09 12:46:19 -0500

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col md:flex-row justify-center items-center p-2 md:p-4 bg-gray-100 text-gray-900">
       <div className="max-w-screen-2xl p-2 flex flex-col md:flex-row items-stretch gap-4">
-        <div className="mb-4">
+        <div className="max-w-fit md:flex-1">
           <SummaryView />
         </div>
-        <div className="w-full md:flex-[3] min-w-[28rem] mb-4">
+        <div className="w-full md:flex-[3]">
           <ExperiencesView />
         </div>
-        <div className="w-full md:flex-[2]">
+        <div className="w-full md:flex-[1]">
           <SkillsView />
         </div>
       </div>
