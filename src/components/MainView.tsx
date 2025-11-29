b448902f11014d6e899107acd2f55b4db4e4c871
gcwill70 <gcwill70@gmail.com>
2025-11-28 20:42:50 -0500

    publications

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,3 +1,5 @@
+import Publications from "@/components/Publications";
+
 export const MainView = () => {
   return (
     <div className="space-y-12">
@@ -10,19 +12,7 @@ export const MainView = () => {
         </p>
       </div>
 
-      <div className="space-y-3">
-        <h1 className="text-3xl font-bold">Publications</h1>
-        <h2 className="text-2xl font-semibold">
-          <a
-            href="https://doi.org/10.1016/j.jss.2024.112322"
-            className="text-blue-500 hover:underline"
-            target="_blank"
-          >
-            Engineering patterns for Trust and Safety on social media platforms
-          </a>
-        </h2>
-        <p>Cramer, Maxam, and Davis. Journal of Systems and Software 2025.</p>
-      </div>
+      <Publications />
 
       <div className="space-y-3">
         <h1 className="text-3xl font-bold">Education</h1>
---
+++ src/components/Publications.tsx
@@ -0,0 +1,17 @@
+export default () => {
+  return (
+    <div className="space-y-3">
+      <h1 className="text-3xl font-bold">Publications</h1>
+      <h2 className="text-2xl font-semibold">
+        <a
+          href="https://doi.org/10.1016/j.jss.2024.112322"
+          className="text-blue-500 hover:underline"
+          target="_blank"
+        >
+          Engineering patterns for Trust and Safety on social media platforms
+        </a>
+      </h2>
+      <p>Cramer, Maxam, and Davis. Journal of Systems and Software 2025.</p>
+    </div>
+  );
+};
