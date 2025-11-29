01dee9e50f2ce9e9e1d719a9d5bcd223007ee1c5
gcwill70 <gcwill70@gmail.com>
2025-11-28 20:44:26 -0500

    education

---
+++ src/components/Education.tsx
@@ -0,0 +1,72 @@
+export default () => {
+  return (
+    <div className="space-y-3">
+      <h1 className="text-3xl font-bold">Education</h1>
+      <h2 className="text-2xl font-semibold">
+        M.S. in Computer Engineering with Thesis - Purdue University - 2023
+      </h2>
+      <ul className="list-disc list-inside">
+        <li>
+          Thesis Title: Trust & Safety Engineering in Open-source Social Media
+          Platforms
+        </li>
+        <li>
+          First empirical study of T&S Engineering that analyzed 60 GitHub
+          issues in{" "}
+          <a
+            href="https://joinmastodon.org"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+          >
+            Mastodon
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://diasporafoundation.org"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+          >
+            Diaspora
+          </a>
+          . Manuscript is available{" "}
+          <a
+            href="https://doi.org/10.25394/PGS.22673890.v1"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+          >
+            here.
+          </a>
+        </li>
+        <li>
+          Developed{" "}
+          <a
+            href="https://github.com/gcwill70/mining_tool"
+            className="text-blue-500 hover:underline"
+            target="_blank"
+          >
+            mining_tool
+          </a>{" "}
+          in Python to collect and analyze GitHub issue discussions.
+        </li>
+      </ul>
+      <h2 className="text-2xl font-semibold">
+        B.S. Electrical Engineering with Distinction - Purdue University - 2018
+      </h2>
+      <ul className="list-disc list-inside">
+        <li>
+          Focus on software engineering with courses in data structures,
+          algorithms, and object-oriented design.
+        </li>
+        {/* <li>
+            <a
+              href="https://github.com/gcwill70/Senior-Design/tree/master/SensorHub"
+              className="text-blue-500 hover:underline"
+              target="_blank"
+            >
+              SensorHub - Senior Project
+            </a>
+          </li> */}
+      </ul>
+    </div>
+  );
+};
--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,3 +1,4 @@
+import Education from "@/components/Education";
 import Publications from "@/components/Publications";
 
 export default () => {
@@ -14,75 +15,7 @@ export default () => {
 
       <Publications />
 
-      <div className="space-y-3">
-        <h1 className="text-3xl font-bold">Education</h1>
-        <h2 className="text-2xl font-semibold">
-          M.S. in Computer Engineering with Thesis - Purdue University - 2023
-        </h2>
-        <ul className="list-disc list-inside">
-          <li>
-            Thesis Title: Trust & Safety Engineering in Open-source Social Media
-            Platforms
-          </li>
-          <li>
-            First empirical study of T&S Engineering that analyzed 60 GitHub
-            issues in{" "}
-            <a
-              href="https://joinmastodon.org"
-              className="text-blue-500 hover:underline"
-              target="_blank"
-            >
-              Mastodon
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://diasporafoundation.org"
-              className="text-blue-500 hover:underline"
-              target="_blank"
-            >
-              Diaspora
-            </a>
-            . Manuscript is available{" "}
-            <a
-              href="https://doi.org/10.25394/PGS.22673890.v1"
-              className="text-blue-500 hover:underline"
-              target="_blank"
-            >
-              here.
-            </a>
-          </li>
-          <li>
-            Developed{" "}
-            <a
-              href="https://github.com/gcwill70/mining_tool"
-              className="text-blue-500 hover:underline"
-              target="_blank"
-            >
-              mining_tool
-            </a>{" "}
-            in Python to collect and analyze GitHub issue discussions.
-          </li>
-        </ul>
-        <h2 className="text-2xl font-semibold">
-          B.S. Electrical Engineering with Distinction - Purdue University -
-          2018
-        </h2>
-        <ul className="list-disc list-inside">
-          <li>
-            Focus on software engineering with courses in data structures,
-            algorithms, and object-oriented design.
-          </li>
-          {/* <li>
-            <a
-              href="https://github.com/gcwill70/Senior-Design/tree/master/SensorHub"
-              className="text-blue-500 hover:underline"
-              target="_blank"
-            >
-              SensorHub - Senior Project
-            </a>
-          </li> */}
-        </ul>
-      </div>
+      <Education />
 
       <div className="space-y-3">
         <h1 className="text-3xl font-bold">Projects</h1>
