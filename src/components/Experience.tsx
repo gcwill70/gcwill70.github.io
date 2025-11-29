c5647dca7a143b3a055e557f5a5549e9a37ae66f
gcwill70 <gcwill70@gmail.com>
2025-11-28 20:50:17 -0500

    add DT experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -44,6 +44,35 @@ export default () => {
           .
         </li>
       </ul>
+
+      <h2 className="text-2xl font-semibold">
+        <a
+          href="https://developertown.com/"
+          className="text-blue-500 hover:underline"
+          target="_blank"
+        >
+          Senior Software Engineer - DeveloperTown
+        </a>
+      </h2>
+      <ul className="list-disc list-inside">
+        <li>
+          Built social application for customer niche with an admin interface,
+          user safety features, and robust user analytics plugin.
+        </li>
+        <li>
+          Created AI health assessment application in Next.js with robust
+          prompting architecture, design system, and HIPAA protection.
+        </li>
+        <li>
+          Replaced financial process that generates 100% of stakeholder revenue
+          by writing over 20,000 lines of calculation logic with 96% test
+          coverage.
+        </li>
+        <li>
+          Led the delivery of 5 projects on time and under budget by pioneering
+          Agile adoption organization-wide.
+        </li>
+      </ul>
     </div>
   );
 };
